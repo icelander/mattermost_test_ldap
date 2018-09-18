4e61da411e580f5b70c4d4203b816e5c6ce2f7c5
Paul Rothrock <paul@movetoiceland.com>
2018-09-18 11:18:37 -0400

    Committing before branch
    Signed-off-by: Paul Rothrock <paul@movetoiceland.com>

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ mysql -uroot -p#MYSQL_ROOT_PASSWORD < /vagrant/db_setup.sql
 
 rm -rf /opt/mattermost
 
-wget --quiet https://releases.mattermost.com/5.2.0/mattermost-5.2.0-linux-amd64.tar.gz
+wget --quiet https://releases.mattermost.com/5.2.1/mattermost-5.2.1-linux-amd64.tar.gz
 
 tar -xzf mattermost*.gz
 
@@ -44,19 +44,18 @@ cp /vagrant/mattermost.service /lib/systemd/system/mattermost.service
 systemctl daemon-reload
 
 cd /opt/mattermost
-bin/mattermost user create --email admin@planetexpress.com --username admin --password admin
-bin/mattermost team create --name planet-express --display_name "Planet Express"
-bin/mattermost team add planet-express admin@planetexpress.com
+# bin/mattermost user create --email admin@planetexpress.com --username admin --password admin
+# bin/mattermost team create --name planet-express --display_name "Planet Express" --email "professor@planetexpress.com"
+# bin/mattermost team add planet-express admin@planetexpress.com
 
 service mysql start
 service mattermost start
 
-IP_ADDR=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
+# IP_ADDR=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
 
-chmod +x /vagrant/update_leela.sh
 printf '=%.0s' {1..80}
 echo 
 echo '                     VAGRANT UP!'
-echo "GO TO http://${IP_ADDR}:8065 and log in with \`fry\`"
+echo "GO TO http://127.0.0.1:8065 and log in with \`professor\`"
 echo
 printf '=%.0s' {1..80}
